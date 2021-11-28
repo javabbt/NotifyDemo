5ecaca093e682ad7238fc6b332adb283a72be171
Yannick Loic <ngapmennyamiyannickloic@gmail.com>
2021-11-28 07:18:18 +0100

    Initial commit

---
+++ app/src/main/java/com/ebookfrenzy/notifydemo/ResultActivity.kt
@@ -0,0 +1,17 @@
+package com.ebookfrenzy.notifydemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.ebookfrenzy.notifydemo.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
